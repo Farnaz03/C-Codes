98a945765f678b6aeb787ba07e6fc5cd8c4717b6
Farnaz Baksh <farnaz.baksh@gmail.com>
2017-12-06 01:58:54 -0400

    collatz

---
+++ Collatz.c
@@ -0,0 +1,42 @@
+/* The Collatz conjecture */
+
+/* The Collatz conjecture is applies to positive integers and 
+speculates that it is always possible to get “back to 1” if 
+you follow these steps:
+•If nis 1, stop.
+•Otherwise, if nis even, repeat this process on n/2.
+•Otherwise, if nis odd, repeat this process on 3n+ 1.*/
+
+//FarnazBaksh - December 2017//
+
+#include<stdio.h>
+
+int collatz (int n);
+ 
+int main ()
+ {
+ 	int n =50;
+ 	int abc;
+ 	
+ 	abc = collatz(n);
+ 	
+ 	printf("Collatz: %d ", abc);
+ 	
+ 	return 0;
+ }
+ 
+int collatz (int n)
+{	
+	//base case
+    if (n==1)
+    	return 0;
+    	
+    //even numbers
+    else if ((n%2)==0)
+    	return 1 + collatz(n/2);
+    
+    //odd numbers
+    else
+    	 return 1 + collatz(3*n+1);
+    	 
+ }
